b0a03232b17a5b7b491424cd84b5f2bcbd734859
Sucre Cando <scando@dataprotect.com.ec>
2020-11-23 12:03:32 -0500

    Creacion boton 'NewProfile' en modulo 'Sending Profile'

--- src/views/app/sending/index.vue
+++ src/views/app/sending/index.vue
@@ -1,3 +1,24 @@
 <template>
-	<router-view/>
+	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<div class="row">
+			<h1 class="page-header">
+				Sending Profiles
+			</h1>
+		</div>
+
+		<div id="flashes" class="row"></div>
+		<div class="row">
+			<b-button variant="primary" @click="$bvModal.show('modalTemplate-1')">+ New Profile</b-button>
+			
+		</div>
+		&nbsp;
+		
+		<div id="emptyMessage" class="row" >
+			<div class="alert alert-info">
+				No profiles yet. Let's create one!
+			</div>
+		</div>
+
+	</div>
+
 </template>
