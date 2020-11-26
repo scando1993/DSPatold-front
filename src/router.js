41c025b0e960d1e3af624eeb470b5b1ca3d4ad05
Sucre Cando <scando@dataprotect.com.ec>
2020-11-25 21:09:20 -0500

    Creacion de tabs dentro de modulo 'Account Settings'

--- src/router.js
+++ src/router.js
@@ -41,10 +41,10 @@ const routes = [
         path: "/app/users",
         component: () => import("./views/app/users"), //users
         children: [
-          {
-            path: "",
-            component: () => import("./views/app/users/users_groups.refactor")
-          },
+          //{
+          //  path: "",
+          //  component: () => import("./views/app/users/users_groups.refactor")
+         // },
         ]
       },
       {
--- src/views/app/account/index.vue
+++ src/views/app/account/index.vue
@@ -1,3 +1,23 @@
 <template>
-	<router-view/>
+	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<div class="row">
+			<h1 class="page-header">Settings</h1>
+		</div>
+		<div>
+			<b-tabs content-class="mt-3">
+				<b-tab title="Account Settings" active><p>I'm the first tab</p></b-tab>
+				<b-tab title="UI Settings"><p>I'm the second tab</p></b-tab>
+				<b-tab title="Reporting Settings"><p>I'm a disabled tab!</p></b-tab>
+			</b-tabs>
+		</div>
+
+		
+	
+			
+		&nbsp;
+
+		
+	</div>
+
+	
 </template>
